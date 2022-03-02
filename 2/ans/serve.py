116e2f59cc7beb9b9d633f76505ddaf7c43c4c4b
to-yuki <to-yuki@jtp.co.jp>
2022-03-02 14:31:02 +0900

    commit

---
+++ 2/ans/serve.py
@@ -0,0 +1,19 @@
+from flask import Flask,jsonify,abort,make_response,request
+from waitress import serve
+import subprocess
+
+api = Flask(__name__)
+@api.route('/create',methods=['GET'])
+def create():
+    subprocess.run(['python3','convertE2Y.py'],shell=False)
+    print('yamlファイルを作成しました。')
+    return make_response('yaml create success')
+
+@api.route('/ansible',methods=['GET'])
+def ansible():
+    subprocess.run(['ansible-playbook','main.yaml'],shell=False)
+    print('Ansibleを実行しました。')
+    return make_response('ansible run success')
+
+if __name__ == '__main__':
+    serve(api,host='0.0.0.0',port=3000)
